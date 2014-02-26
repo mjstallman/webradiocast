945c8ef9212fb3c9f55421eaf2100975c32d96ce
attakei <attakei@gmail.com>
2014-02-26 23:07:30 +0900

    refactor

--- webradiocast/scripts/build_xml.py
+++ webradiocast/scripts/build_xml.py
@@ -14,7 +14,7 @@ Parser.add_argument('-o', dest='output', help='output to.')
 Parser.add_argument(dest='config_path')
 
 
-def main(argv=sys.argv):
+def main():
     args = Parser.parse_args()
     if args.config_path is None:
         Parser.usage()
@@ -22,15 +22,15 @@ def main(argv=sys.argv):
     config = ConfigParser.SafeConfigParser()
     config.read(os.path.abspath(args.config_path))
         
-    manager_ = FeedManager(os.path.abspath(args.playlist))
+    manager = FeedManager(os.path.abspath(args.playlist))
     if args.media is None:
-        sys.stderr.write('media is reqyured.\n')
+        sys.stderr.write('media is required.\n')
         exit(0)
-    elif not manager_.has_media(args.media):
+    elif not manager.has_media(args.media):
         sys.stderr.write('media "{0}" is not found.\n'.format(args.media))
         exit(1)
 
-    builder = manager_.get_builder(args.media)
+    builder = manager.get_builder(args.media)
     builder.make_feed()
     print(builder.document.toprettyxml('  '))
     exit(0)
